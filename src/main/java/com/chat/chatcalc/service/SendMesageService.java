ba9b606054e3cf802f2e33ff1baef6042be5e60f
AkyrisPinange <48262372+AkyrisPinange@users.noreply.github.com>
2023-09-14 21:50:37 -0300

    url aplication.proporteis

--- src/main/java/com/chat/chatcalc/service/SendMesageService.java
+++ src/main/java/com/chat/chatcalc/service/SendMesageService.java
@@ -1,20 +1,12 @@
 package com.chat.chatcalc.service;
 
-import com.chat.chatcalc.entiteis.Chats;
 import com.chat.chatcalc.entiteis.User;
-import com.chat.chatcalc.enums.MessageType;
 import com.chat.chatcalc.model.ChatMessage;
 import com.chat.chatcalc.model.SendMessage;
 import com.chat.chatcalc.reporsitory.ChatMessageRepository;
-import com.chat.chatcalc.reporsitory.ChatsRepository;
 import com.chat.chatcalc.reporsitory.UserRepository;
-import com.github.f4b6a3.uuid.UuidCreator;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.UUID;
 
 @Service
 public class SendMesageService {
