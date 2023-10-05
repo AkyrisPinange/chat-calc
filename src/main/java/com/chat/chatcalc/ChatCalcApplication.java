a362b111a7a968c99af1513c5f74d2c0840a216a
AkyrisPinange <48262372+AkyrisPinange@users.noreply.github.com>
2023-10-05 20:43:15 -0300

    J

--- src/main/java/com/chat/chatcalc/ChatCalcApplication.java
+++ src/main/java/com/chat/chatcalc/ChatCalcApplication.java
@@ -1,5 +1,7 @@
 package com.chat.chatcalc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -8,8 +10,13 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 public class ChatCalcApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ChatCalcApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChatCalcApplication.class, args);
+		LOGGER.info("*******************VESÃO 1 *******************************************");
 	}
+
+	
 
 }
