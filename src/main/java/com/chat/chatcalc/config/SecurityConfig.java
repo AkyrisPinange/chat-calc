e78856e67dc9d6aba713a32941ec3bba1abec084
AkyrisPinange <48262372+AkyrisPinange@users.noreply.github.com>
2024-06-10 20:43:30 -0300

    Fixed permition websoket

--- src/main/java/com/chat/chatcalc/config/SecurityConfig.java
+++ src/main/java/com/chat/chatcalc/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/signup", "/signin").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/test/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/ws").permitAll()
 //                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 )
